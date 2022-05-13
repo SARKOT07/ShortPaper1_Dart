45d470dc0cc20ddb5ee617622f3c8837c124576c
Francogavidia <francogavidia027@gmail.com>
2022-05-12 23:23:01 -0400

    first commit

---
+++ bin/shortpaper1_dart.dart
@@ -0,0 +1,5 @@
+import 'package:shortpaper1_dart/shortpaper1_dart.dart' as shortpaper1_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shortpaper1_dart.calculate()}!');
+}
---
+++ test/shortpaper1_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:shortpaper1_dart/shortpaper1_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
